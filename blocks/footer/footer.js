7492ad07cdfe3258b1caff6676d77063f5475520
svfranklindemo <166366015+svfranklindemo@users.noreply.github.com>
2024-11-28 13:55:44 +0530

    Initial commit

---
+++ blocks/footer/footer.js
@@ -0,0 +1,23 @@
+import { getMetadata } from '../../scripts/aem.js';
+import { loadFragment } from '../fragment/fragment.js';
+
+/**
+ * loads and decorates the footer
+ * @param {Element} block The footer block element
+ */
+export default async function decorate(block) {
+  // load footer as fragment
+  const footerMeta = getMetadata('footer');
+  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
+  const fragment = await loadFragment(footerPath);
+
+  // decorate footer DOM
+  block.textContent = '';
+  const footer = document.createElement('div');
+
+  if (fragment) {
+    while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+  }
+
+  block.append(footer);
+}
---
+++ scripts/delayed.js
@@ -0,0 +1,5 @@
+import { sampleRUM } from './lib-franklin.js';
+
+sampleRUM('lpl-eds');
+
+// add more delayed functionality here
